28ede5231011dea8a4709d10e882aeb0816c8681
Michael Teschner <120539187+BlazeTechDev@users.noreply.github.com>
2025-10-28 09:37:46 -0500

    slightly farther draw distance

--- src/gameplay/components/foliage_component.hpp
+++ src/gameplay/components/foliage_component.hpp
@@ -6,9 +6,9 @@
 
 struct FoliageComponent {
     //must be a perfect square
-    glm::ivec2 numInstancesPerAxis = glm::ivec2(400);
-    glm::ivec2 foliageChunkDimensions = glm::ivec2(200);
-    float cameraCutoffDistance = 200.0f;
+    glm::ivec2 numInstancesPerAxis = glm::ivec2(500);
+    glm::ivec2 foliageChunkDimensions = glm::ivec2(250);
+    float cameraCutoffDistance = 500.0f;
     float breezeAmount = 0.25f;
     float windAmount = 25.0f;
     float windAngle = 0.0f;
